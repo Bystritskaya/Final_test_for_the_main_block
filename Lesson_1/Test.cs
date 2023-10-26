c22474048b0981235ca7ad5612069eb2f0b36c4b
Bystritskaya <marbystritskaya@gmail.com>
2023-10-26 06:01:11 +0300

    Created solution

--- Lesson_1/Test.cs
+++ Lesson_1/Test.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+Console.Write("Введите элементы массива через запятую: ");
+string[] input = Console.ReadLine().Split(',');
+
+string[] output = new string[input.Length];
+int count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        output[count] = input[i];
+        count++;
+    }
+}
+
+Console.WriteLine("Результат:");
+for (int i = 0; i < count; i++)
+{
+    Console.Write(output[i] + " ");
+}
